31634665e5388ddc8b85961dfc6f66f1957d5482
Kennylovetheworld <944545895@qq.com>
2019-10-17 22:34:10 -0400

    rnn finished

--- data_try.py
+++ data_try.py
@@ -7,10 +7,14 @@ import torch.optim as optim
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import PorterStemmer
+# from torchsummary import summary
+# from torch.utils.data import DataLoader, TensorDataset
 
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 GLOVE_FILE = 'glove.6B/glove.6B.300d.txt'
 VEC_LENGTH = 300
-PADDING_LENGTH = 30
+PADDING_LENGTH = 800
+BATCH_SIZE = 20
 TRAINING_FILE = 'wsj1-18.training'
 VALIDATION_FILE = 'wsj19-21.truth'
 NEG_Path = "dataset/training/neg/"
@@ -24,15 +28,17 @@ class data_process():
         self.glove_model = glove_model
 
     def data_embedding(self, neg_s, pos_s):
-        outname = 'log/data.pkl'
+        outname = 'log/data_doc_'+str(PADDING_LENGTH)+'.pkl'
         if os.path.isfile(outname):
             data = pickle.load(open(outname,'rb'))
         else:
             label = []
-            embedded_sents = [] 
+            embedded_sents = []
+            lens = []
             for sent in neg_s:
-                label.append(0)
+                label.append([0,1])
                 sent = word_tokenize(sent)
+                lens.append(len(sent))
                 sent_vec = np.zeros((PADDING_LENGTH, VEC_LENGTH))
                 for i, word in enumerate(sent):
                     if i == PADDING_LENGTH:
@@ -46,8 +52,9 @@ class data_process():
                     sent_vec[i] = vec
                 embedded_sents.append(sent_vec)
             for sent in pos_s:
-                label.append(1)
+                label.append([0,1])
                 sent = word_tokenize(sent)
+                lens.append(len(sent))
                 sent_vec = np.zeros((PADDING_LENGTH, VEC_LENGTH))
                 for i, word in enumerate(sent):
                     if i == PADDING_LENGTH:
@@ -66,8 +73,33 @@ class data_process():
             data['label'] = label
             data['features'] = embedded_sents
             pickle.dump(data, open(outname,'wb'))
+            print(np.array(lens).std())
         return data
 
+    def doc_tokenize(self):
+        neg_outname = 'log/neg_docs.pkl'
+        pos_outname = 'log/pos_docs.pkl'
+        # Fetch NEG doc
+        if os.path.isfile(neg_outname):
+            neg_d = pickle.load(open(neg_outname,'rb'))
+        else:
+            neg_d = []
+            for file_name in NEG_Files:
+                with open(NEG_Path + file_name, encoding='windows-1252') as file:
+                    data = file.read()
+                    neg_d.append(data)
+            pickle.dump(neg_d, open(neg_outname,'wb'))
+        # Fetch POS doc
+        if os.path.isfile(pos_outname):
+            pos_d = pickle.load(open(pos_outname,'rb'))
+        else:
+            pos_d = []
+            for file_name in POS_Files:
+                with open(POS_Path + file_name, encoding='windows-1252') as file:
+                    data = file.read()
+                    pos_d.append(data)
+            pickle.dump(pos_d, open(pos_outname,'wb'))
+        return neg_d, pos_d
 
     def sentence_tokenize(self):
         neg_outname = 'log/neg_sents.pkl'
@@ -114,8 +146,20 @@ def loadGloveModel(gloveFile):
 class RNN(nn.Module):
     def __init__(self):
         super(RNN, self).__init__()
-        self.rnn = nn.LSTM(input_size = 300, hidden_size = 45, bidirectional=True)
-        self.linear = nn.Linear(90, 45)
+        self.rnn = nn.LSTM(input_size = 300, hidden_size = 50, bidirectional=True)
+        self.linear = nn.Linear(100, 2)
+        self.softmax = nn.Softmax(-1)
+    
+    def forward(self, seq, hidden=None):
+        output, _ = self.rnn(seq)
+        output = self.softmax(self.linear(output))
+        return output
+
+class DAN(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.linear1 = nn.Linear(200, 200)
+        self.linear2 = nn.Linear(200, 2)
         self.softmax = nn.Softmax(-1)
     
     def forward(self, seq, hidden=None):
@@ -125,35 +169,36 @@ class RNN(nn.Module):
 
 model = loadGloveModel(GLOVE_FILE)
 dp = data_process(model)
-neg_s, pos_s = dp.sentence_tokenize()
-data = dp.data_embedding(neg_s, pos_s)
+neg_d, pos_d = dp.doc_tokenize()
+data = dp.data_embedding(neg_d, pos_d)
 labels = data['label']
-inputs = data['features']
+features = data['features'].swapaxes(0,1)
 
-RNN = RNN()
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+net = RNN().to(DEVICE)
+criterion = nn.MSELoss()
+optimizer = optim.Adam(net.parameters(), lr=0.0001)
 
+# train_data = TensorDataset(torch.from_numpy(features), torch.from_numpy(labels))
+# train_loader = DataLoader(train_data, shuffle=True, batch_size= BATCH_SIZE)
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
-for epoch in range(2):
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
-        optimizer.zero_grad()
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
+running_loss = 0.0
+optimizer.zero_grad()
+for iteration in range(1000):
+    #training
+    rand_indexs = list(np.random.random_integers(0, labels.shape[0] - 1, BATCH_SIZE))
+    x = features[:, rand_indexs] 
+    y = labels[rand_indexs]
+    # forward + backward + optimize
+    x = torch.tensor(x).float().reshape(-1, BATCH_SIZE, 300).to(DEVICE)
+    y = torch.tensor(y).reshape(-1, 2).to(DEVICE)
+    outputs = net(x)[-1,:,:]
+    loss = criterion(y, outputs)
+    loss.backward()
+    optimizer.step()
+    # print statistics
+    running_loss += loss.item()
+    if (iteration+1) %10 == 0:
+        print('[%d] loss: %.3f' %(iteration + 1, running_loss/20))
+        running_loss = 0.0
 
 print('Finished Training')
